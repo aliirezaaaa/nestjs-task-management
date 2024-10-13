675f337951e91f79384185f4797143da2a7ba254
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-10-13 21:59:02 +0330

    Error handling for task module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from 'src/tasks/tasks.service';
 
 import { Task, TaskStatus } from 'src/tasks/task.model';
 import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
+import { UpdateTaskStatusDto } from 'src/tasks/dto/update-task-status.dto';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
@@ -28,7 +29,8 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task{
+  updateTaskStatus(@Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Task{
+    const {status} = updateTaskStatusDto
     return this.tasksService.updateTaskStatus(id,status)
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
 import { Task, TaskStatus } from 'src/tasks/task.model';
 import {v4 as uuid} from "uuid"
@@ -12,7 +12,11 @@ export class TasksService {
 	}
     
 	getTaskById(id:string) :Task {
-		return this.tasks.find(task=> task.id===id)
+		const found = this.tasks.find(task => task.id === id)
+		if (!found) {
+			throw new NotFoundException(`The task with id: ${id}, not found! `) 
+		}
+		return found
 	}
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -29,7 +33,8 @@ export class TasksService {
 	}
 	
 	deleteTaskById(id: string): void{
-		this.tasks = this.tasks.filter(task=>task.id !== id)
+		const found = this.getTaskById(id)
+		this.tasks = this.tasks.filter(task=>task.id !== found.id)
 	}
 
 	updateTaskStatus(id: string, taskStatus: TaskStatus):Task {
